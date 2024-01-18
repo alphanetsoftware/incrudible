1546886748d48721f8767e203f33a474cf0f4952
louisjasek <36529635+louisjasek@users.noreply.github.com>
2024-01-19 09:10:53 +1100

    Re-arranged folders again

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ connection="$1"
 password="$2"
 
 # Run the sql statements, passing the password as a parameter
-./run_sql_statements.sh create_database.sql "$connection" postgres "$password"
-./run_sql_statements.sh create_objects.sql "$connection" incrudible "$password"
-./run_sql_statements.sh create_procedures.sql "$connection" incrudible "$password"
+./sql/run_sql_statements.sh create_database.sql "$connection" postgres "$password"
+./sql/run_sql_statements.sh create_objects.sql "$connection" incrudible "$password"
+./sql/run_sql_statements.sh create_procedures.sql "$connection" incrudible "$password"
